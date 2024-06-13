8141b0340f41121a9e294350d79a21d95ba28068
SmolChicken45 <98850340+SmolChicken45@users.noreply.github.com>
2024-06-13 10:43:25 -0400

    petit commentaire pour les années futur

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -22,9 +22,25 @@ public class Swerve extends SubsystemBase {
       };
   }
   public void drive(double[] angles, double[] magnitudes){
+    /*
+     * Cher programmeur du futur de probablement l'année 2024-25
+     * ceci est la liste à faire du code en temps du 13/06/2024
+     * il faut ajouter un décalage du Navx (le gyro) aux angles des vecteurs 
+     * de chacun des modules pour que le robot soit stage centric au lieu d'être 
+     * robot centric.
+     * 
+     * De plus, les encodeur qui vérifie l'angle des moteurs semble se décaler
+     * pour régler ce problème il faut soit utiliser Pheonix Tuner X pour les remettre à 0
+     * ou trouver une solution pour ne pas qu'il se décale.
+     * 
+     * Si vous voulez utiliser ce code pour le robot puisqu'il est sensiblement complet, 
+     * copier le c'est bien correct
+     * 
+     * -Noah Pitre
+     */
     /*distribue les vecteurs a leur module respectif*/
     for(int i= 0; i<4; i++){
-      mSwervemods[i].setModuleSpeed(magnitudes[i], angles[i]);
+      mSwervemods[i].setModuleSpeed(magnitudes[i], angles[i]/*Décalage avec le Navx, angles sont en radians */);
       SmartDashboard.putNumber("angle "+ i, angles[i]);
       SmartDashboard.putNumber("magnitude " + i, magnitudes[i]);
     }
